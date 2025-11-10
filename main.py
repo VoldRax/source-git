882b37d0a33bf1449e1f6db7137f46390b8ec768
Rudra Bajpai <rudravoldigoad@gmail.com>
2025-11-10 07:51:45 +0530

    commit no. 1621

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ print(str)
 
 arr_1=[3,5,7,9]
 print(7 in arr_1)
+
+class It2:
+    def __init__(self, n):
+        self.i=0
+        self.n=n
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.i>=self.n:
+            raise StopIteration
+        self.i+=1
+        return self.i-1
+print(list(It2(3)))
