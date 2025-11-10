cb7a96293723122efc80d50f52f594d03f2b35f2
Rudra Bajpai <rudravoldigoad@gmail.com>
2025-11-10 07:36:15 +0530

    commit no. 1619

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-str = '0hx-+1-o %bz$uug#;(h-19gyli)sm+i)y(vf(-$0_t)(n0o!k$j59-4o#4,:js-3'
+str = '0-:-m]#th}5 k$;(d+{j'+k#]ju[:# *6rkiygri<nu?}_ff?u?9z_q4sy1}y;(*&'
 print(str)
-
-unique_1 = {1, 2, 2, 3}
-unique_1.add(1)
-print(f"Generated set #1: {unique_1}")
-
-lst_1 = [1, 2, 3]
-lst_1.append(4)
-lst_1.reverse()
-print(f"Modified list_1: {lst_1}")
